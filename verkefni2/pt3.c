0449446cabef2a8d868dc441935e1700e30f447d
Kristjan-O-Ragnarsson <kristjanra262@nemi.tskoli.is>
2018-10-18 09:54:28 +0000

    turn function

---
+++ verkefni2/pt3.c
@@ -0,0 +1,75 @@
+#pragma config(Sensor, dgtl9,  rightEncoder,        sensorQuadEncoder)
+#pragma config(Sensor, dgtl11,  leftEncoder,         sensorQuadEncoder)
+#pragma config(Motor,  port2,           rightMotor,    tmotorNormal, openLoop)
+#pragma config(Motor,  port3,           leftMotor,     tmotorNormal, openLoop, reversed)
+
+
+/*----------------------------------------------------------------------------------------------------*\
+|*                                 - ADVANCED Movement by Rotation -                                  *|
+|*                                      ROBOTC on VEX 2.0 CORTEX                                      *|
+|*                                                                                                    *|
+|*  This program uses functions 'TurnLeft(float)' and 'Forward(float)' to very neatly and modularly   *|
+|*  run your robot in a square.  There is a two second pause at the beginning of the program.         *|
+|*                                                                                                    *|
+|*                                        ROBOT CONFIGURATION                                         *|
+|*    NOTES:                                                                                          *|
+\*-----------------------------------------------------------------------------------------------4246-*/
+
+const float TURNBASE = 3.335;
+// Functions Prototypes
+void Forward(float r);
+void TurnLeft(int deg);
+
+// Declare Global Variables
+const float rotations = 360.0;
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++| MAIN |+++++++++++++++++++++++++++++++++++++++++++++++
+task main()
+{
+  wait1Msec(2000);
+
+  int i;
+  for(i=0; i<4; i++)
+  {
+    Forward(1.0);
+    TurnLeft(90);
+  }
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+//--------------------------------------------| FORWARD |---------------------------------------------
+void Forward(float r)
+{
+  SensorValue[rightEncoder] = 0;
+  SensorValue[leftEncoder]  = 0;
+
+  // While the encoders have not yet met their goal: (r * rotations) ie (3.0 * 360.0) or "three rotations"
+  while(SensorValue[rightEncoder] < (r * rotations) && SensorValue[leftEncoder] < (r * rotations))
+  {
+    motor[rightMotor] = 63;
+    motor[leftMotor]  = 63;
+  }
+  motor[rightMotor] = 0;
+  motor[leftMotor]  = 0;
+}
+//----------------------------------------------------------------------------------------------------
+
+//-------------------------------------------| TURN LEFT |--------------------------------------------
+void TurnLeft(int deg)
+{
+  SensorValue[rightEncoder] = 0;
+  SensorValue[leftEncoder]  = 0;
+
+  // While the encoders have not yet met their goal: (left is compared negativly since it will in reverse)
+  while(SensorValue[rightEncoder] < (deg * TURNBASE) && SensorValue[leftEncoder] > (-1 * deg * TURNBASE))
+  {
+    motor[rightMotor] = 63;
+    motor[leftMotor]  = -63;
+  }
+  motor[rightMotor] = 0;
+  motor[leftMotor]  = 0;
+}
+//----------------------------------------------------------------------------------------------------
