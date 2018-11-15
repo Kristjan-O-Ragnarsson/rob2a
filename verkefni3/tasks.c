a31e90dabcda1391039c451c918a95aea45ad2d7
Kristjan-O-Ragnarsson <kristjanra262@nemi.tskoli.is>
2018-11-15 08:57:05 +0000

    duno what has changes

--- verkefni3/tasks.c
+++ verkefni3/tasks.c
@@ -3,7 +3,7 @@ task StopIfNeed()
 {
 	while(true)
 	{
-		if( vexRT[Btn8D] == 1 && vexRT[Btn7D] == 1 || SensorValue[touchsensor] == 1)
+		if( vexRT[Btn8D] == 1 && vexRT[Btn7D] == 1 || SensorValue[touchSensor] == 1)
 		{
       StopAllTasks();
    	}
