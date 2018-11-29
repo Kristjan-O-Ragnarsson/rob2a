ea947f3a56d486b7673b4bb8d1db0cdebe59bc4c
Kristjan-O-Ragnarsson <kristjanra262@nemi.tskoli.is>
2018-11-29 08:33:13 +0000

    verk6 base

---
+++ verkefni6/verkefni6.c
@@ -0,0 +1,24 @@
+#pragma config(Sensor, dgtl1,  sonarSensor,         sensorSONAR_cm)
+#pragma config(Sensor, in4,    lightSensor,         sensorReflection)
+#pragma config(Sensor, dgtl8,   touchSensor,			sensorTouch)
+#pragma config(Motor,  port2,           rightMotor,    tmotorNormal, openLoop)
+#pragma config(Motor,  port3,           leftMotor,     tmotorNormal, openLoop, reversed)
+#pragma config(Motor,  port6,           clawMotor,     tmotorVex269, openLoop, reversed)
+#pragma config(Motor,  port7,           armMotor,      tmotorVex393, openLoop, reversed)
+#pragma config(UART_Usage, UART2, uartVEXLCD, baudRate19200, IOPins, None, None)
+#include "../verkefni3/tasks.c"
+
+
+task main()
+{
+	StartTask(StopIfNeed);
+	StartTask(BAtteryDisplay);
+	while(true){
+		if(SensorValue(lightSensor) < 300)
+		{
+
+		}
+	}
+
+
+}
